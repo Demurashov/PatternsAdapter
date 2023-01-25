1d0002f5f3f7bdcc8d9d0108b625a7234c54ed3e
Demurashov <denmurolimp@yandex.ru>
2023-01-25 16:30:59 +0300

    V1

---
+++ src/test/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       //работа калькулятора напрямую
+        Calculator calc = new Calculator();
+        System.out.println(
+                calc.newFormula()
+                        .addOperand(5)
+                        .addOperand(15)
+                        .calculate(Calculator.Operation.MULT)
+                        .result()
+        );
+        Ints calcInts=new IntsCalculator();
+        System.out.println("Суммирование: "+calcInts.sum(2,3));
+        System.out.println("Умножение: "+calcInts.mult(2,3));
+        System.out.println("Возведение в степень:"+calcInts.pow(2,3));
+
+    }
+}
